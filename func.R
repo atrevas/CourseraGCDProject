6fed8e47a79831e8daf38f7f2f46ec62de77a317
André das Trevas <atrevas@gmail.com>
2014-07-19 08:52:51 -0300

    File func.R created.

--- func.R
+++ func.R
@@ -1,7 +1,3 @@
-# make.names
-
-# 1 - Merges the training and the test sets to create one data set.
-
 DfMergeTrainTest <- function () {
   # Initialize folder variables
   cDataFolder <- file.path('.', 'data', 'UCI HAR Dataset')
@@ -47,18 +43,3 @@ DfMergeTrainTest <- function () {
   
   return (dfAll)
 }
-
-
-dfHAR <- DfMergeTrainTest()
-
-# 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
-
-
-
-
-
-# 3 - Uses descriptive activity names to name the activities in the data set
-# 4 - Appropriately labels the data set with descriptive variable names. 
-# 5 - Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-
